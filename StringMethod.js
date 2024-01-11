83f7f7ff6cf3dded122e54c896d12da9922949e5
Mehedi <mehedi.csebu029@gmail.com>
2024-01-11 22:56:31 +0600

    String methods practice code

--- StringMethod.js
+++ StringMethod.js
@@ -8,3 +8,18 @@ console.log(str2);
 
 str2 = str.substring(6,14)
 console.log(str2);
+
+str3 = str.substr(7,8) //substr(start, length of slice)
+console.log(str3);
+
+console.log(str.toLowerCase()); //toLowerCase() convert lowercase
+console.log(str.toUpperCase()); //toUpperCase() convert UpperCase
+
+let text1 = "Hello";
+let text2 = "World";
+console.log(text1+" "+text2);
+console.log(text1.concat(" "+text2)); //str.concat() concatenate the strings
+
+console.log(str.trim()); //trim() method remove whitespace from both end of string
+
+console.log(text1.split("")); //split convert a string into an array
